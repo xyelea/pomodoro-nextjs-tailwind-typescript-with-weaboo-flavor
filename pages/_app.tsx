5399a6d72709006cdc4e014b1d2618c99e649bfe
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-01-19 08:25:02 +0700

    add analisis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
